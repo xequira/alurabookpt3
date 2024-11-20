107726552b1dad7978c9dd19c9e9b5d0d81d25f5
xequira <amanda.terto20@gmail.com>
2024-11-20 03:17:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Cultura POP',
+    'Quem é o Homem de Ferro?',
+    'Tony Stark é o Homem de Ferro'
+)
+
+criaCartao(
+    'Biologia',
+    'Como a aranha armadeira é conhecida?',
+    'Aranha das Bananeiras'
+)
+
+criaCartao(
+    'Cultura POP',
+    'Quem é o Darth Vader?',
+    'Anakin Skywalker'
+)
+
+criaCartao(
+    'Música',
+    'Sertanejo raiz usa qual instrumento de cordas?',
+    'Viola Caipira'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é uma tromba d´água?',
+    'Fenômeno meteorológico que ocorre quando um vórtice de ar gira rapidamente sobre a superfície de um corpo de água'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual esporte Forrest Gump jogou para o exército?',
+    'Ping-Pong'
+)
